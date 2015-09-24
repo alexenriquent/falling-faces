3a11a8447643228ad4c25d15291372a92be05e00
Thanat Chokwijitkul <alexenriquent@gmail.com>
2015-09-24 16:59:11 +1000

    Faces random movement

--- FallingFaces/fallingfaces.c
+++ FallingFaces/fallingfaces.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -20,9 +21,9 @@ int left_button_pressed();
 int right_button_pressed();
 int check_option(int option);
 void menu(int level, char desc[], char buff[]);
-void score_bar(int lives, int scores, char buff[]);
-
+void status(int lives, int scores, char buff[]);
 void update_player(Sprite* player);
+int faces_collision(Sprite* face1, Sprite* face2);
 
 unsigned char player_bitmaps[BYTE_PER_PLAYER] = {
   0b00000000,
@@ -94,6 +95,7 @@ unsigned char faces_bitmaps[NUM_FACES][BYTE_PER_FACE] = {
 
 Sprite player;
 Sprite faces[NUM_FACES];
+int start = 0;
 
 int main() {
 
@@ -101,11 +103,12 @@ int main() {
   int lives = 3;
   int scores = 0;
   char buff[80];
+  int random = 0;
 
   init_sprite(&player, 39, 40, 8, 8, player_bitmaps);
-  init_sprite(&faces[0], 12, 10, 16, 16, faces_bitmaps[0]);
+  init_sprite(&faces[0], 10, 10, 16, 16, faces_bitmaps[0]);
   init_sprite(&faces[1], 33, 10, 16, 16, faces_bitmaps[1]);
-  init_sprite(&faces[2], 54, 10, 16, 16, faces_bitmaps[2]);
+  init_sprite(&faces[2], 56, 10, 16, 16, faces_bitmaps[2]);
 
   set_clock_speed(CPU_8MHz);
 
@@ -127,14 +130,34 @@ int main() {
           opt = check_option(opt);
 
           if (left_button_pressed()) {
+            start = 1;
             while (lives > 0) {
               clear_screen();
-              score_bar(lives, scores, buff);
+              status(lives, scores, buff);
               draw_sprite(&player);
               update_player(&player);
 
               for (int i = 0; i < NUM_FACES; i++) {
                 draw_sprite(&faces[i]);
+              }
+
+              for (int i = 0; i < NUM_FACES; i++) {
+                int next_y = (int) round(faces[i].y + 1);
+
+                if (next_y >= 49) {
+                  faces[i].y = 10;
+                  random = rand() % 66;
+                  faces[i].x = random;
+
+                  while (faces_collision(&faces[i], &faces[(i + 1) % 3]) ||
+                    faces_collision(&faces[i], &faces[(i + 2) % 3])) {
+                    random = rand() % 66;
+                    faces[i].x = random;
+                  }
+
+                  faces[i].x = random;
+                  faces[i].y = 10;
+                }
               }
 
               show_screen();
@@ -239,7 +262,7 @@ void menu(int level, char desc[], char buff[]) {
   draw_string(0, 32, "Right: Skip");
 }
 
-void score_bar(int lives, int scores, char buff[]) {
+void status(int lives, int scores, char buff[]) {
   draw_string(0, 0, "L:");
   sprintf(buff, "%d", lives);
   draw_string(12, 0, buff);
@@ -266,12 +289,18 @@ void update_player(Sprite* player) {
   }
 }
 
+int faces_collision(Sprite* face1, Sprite* face2) {
+  return ((abs(face1->x - face2->x) * 2) - 10 < 32);
+  // return (face1->x < face2->x + 23 &&
+  //   face1->x + 23 > face2->x &&
+  //   face1->y < face2->y + 16 &&
+  //   face1->y + 16 > face2->y);
+}
+
 ISR(TIMER1_OVF_vect) {
-  for (int i = 0; i < NUM_FACES; i++) {
-    int next_y = (int) round(faces[i].y + 1);
-    faces[i].y += 2;
-    if (next_y >= 48) {
-      faces[i].y = 10;
+  if (start) {
+    for (int i = 0; i < NUM_FACES; i++) {
+      faces[i].y += 2;
     }
   }
 }
